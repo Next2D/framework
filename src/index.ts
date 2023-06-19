5708d2134afa85445553bdef29c79120ab270e96
ienaga <ienaga@tvon.jp>
2023-06-20 08:15:38 +0900

    #88 TypeScriptでの開発環境の調整

--- src/index.ts
+++ src/index.ts
@@ -3,6 +3,7 @@ import { Application } from "./application/Application";
 import { DefaultLoading } from "./domain/screen/DefaultLoading";
 import { View } from "./view/View";
 import { ViewModel } from "./view/ViewModel";
+import { Content } from "./application/content/Content";
 import { packages } from "./application/variable/Packages";
 import { context } from "./application/variable/Context";
 import { cache } from "./application/variable/Cache";
@@ -11,7 +12,7 @@ import { response } from "./application/variable/Response";
 import { loaderInfoMap } from "./application/variable/LoaderInfoMap";
 
 // output build version
-console.log("%c Next2D Framework %c 1.5.3 %c https://next2d.app",
+console.log("%c Next2D Framework %c 1.5.5 %c https://next2d.app",
     "color: #fff; background: #5f5f5f",
     "color: #fff; background: #4bc729",
     "");
@@ -21,6 +22,7 @@ export {
     DefaultLoading,
     View,
     ViewModel,
+    Content,
     packages,
     context,
     cache,
